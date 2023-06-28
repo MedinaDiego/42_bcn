96142a1c0c4057b50496d3fc62ed6de16fe7b957
Diego Medina Mauricio <dimedina@cbr4s6.42barcelona.com>
2023-06-28 19:14:45 +0200

    test update main

--- libft/main.c
+++ libft/main.c
@@ -1,6 +1,43 @@
+#include <stdio.h>
 #include "libft.h"
 
-void main(void)
+int main()
 {
-    ft_isalpha('a');
+    //ft_isalpha: comprueba si un caracter es un alfabeto o no. (1 verdad - 0 falso)
+    /*
+    char c;
+    c = ' ';
+    
+    if(ft_isalpha(c) == 0)
+        printf("No pertenece al alfabeto");
+    else
+        printf("Si pertenece al alfabeto");
+    */
+
+
+   //ft_isdigit: comprueba si un caracter es un caracter numerico {0-9} o no
+    /*char c;
+    //c = 48;
+    c = '7';
+
+    if(ft_isdigit(c) == 0)
+        printf("No es un caracter numerico entre 0 - 9");
+    else
+        printf("Si es un caracter numerico");    
+    */
+
+   //ft_isalnum: 
+
+   //ft_isprint: comprueba si un caracter es un caracter imprimible o no. Son aquellos caracteres
+   // que ocupan espacio de impresion. Valor de retorno 0 false - 1 true
+
+    //ft_strlen: calcula la longitud de una cadena. 
+
+    char cadena[] = "   0patata";
+    printf("La longitud de la cadena es de %d", ft_strlen(cadena));
+
+    ft_memset(cadena, '*', 15);
+    printf("%s\n" ,cadena);
+    printf("La longitud de la cadena es de %d\n", ft_strlen(cadena));
+    return (0);
 }
